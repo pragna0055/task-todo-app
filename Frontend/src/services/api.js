9eef1d6890965ea8d1a372561f5e8e23803b74ff
pragna0055 <pragna0055@gmail.com>
2026-05-31 18:14:24 +0530

    fix api url

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-todo-app-backend.onrender.com",
+  baseURL: "https://task-todo-app-backend.onrender.com/api",
 });
 
 export default API;
