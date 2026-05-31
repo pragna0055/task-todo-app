88b57e113cf6410d1f7f0b9fdc337cce28c353f1
pragna0055 <pragna0055@gmail.com>
2026-05-31 18:06:37 +0530

    Connect frontend to deployed backend

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://task-todo-app-backend.onrender.com",
 });
 
 export default API;
